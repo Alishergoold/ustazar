1adb3508589ec3fe13ac2f15f19888379eb967aa
Alishergoold <alishergoold1989@gmail.com>
2021-07-31 16:17:47 +0500

    Add FunctionalComponent

--- src/container/App.js
+++ src/container/App.js
@@ -1,18 +1,84 @@
-import React from 'react';
-import { Main } from '../styles/Main';
-import Header from './Header/Header';
-import NewProducts from './New Products/NewProducts';
+////PROPS
+//const App = (props) => {
+//  return (
+//    <div>
+//      {console.log(props)}
+//      <h1 >Hello World {props.name}</h1>
+//    </div>
+//  )
+//}
+//export default App;
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/App.css';
 
-function App() {
+// functional componentlarda arreylarni map qilish.
+//const arr = [
+//  {
+//    firstName: 'Alisher',
+//    lastName: 'Raximov'
+//  },
+//  {
+//    firstName: 'Bahodir',
+//    lastName: 'Karimov'
+//  },
+//  {
+//    firstName: 'Valijon',
+//    lastName: 'Shamsiyev'
+//  }
+//]
+
+//function App(props) {
+//  return (
+//    <div>
+//      {console.log(props)}
+//      <h1 >Hello World {props.name}</h1>
+
+//      {
+//        arr.map((item, index) => <p key={index}>{item.firstName + ' ' + item.lastName}</p>)
+//      }
+//    </div>
+//  )
+//}
+//export default App;
+
+
+
+
+// function event
+//function App(props) {
+//  function btnClicked() {
+//    console.log(props.name)
+//  }
+//  return (
+//    <div>
+//      <h1>Hello {props.name}</h1>
+
+//      <button onClick={btnClicked}>Check</button>
+//    </div>
+//  )
+//}
+//export default App;
+
+
+
+// Parent and child Component
+import ChildComponent from "./ChildComponent";
+function App(props) {
+
+  function drawArray() {
+    return arr.map((item, index) => <p key={index}>It is {item}</p>)
+  }
+
+  let arr = [1, 2, 3, 4, 5]
+
   return (
-    <Main>
-      <Header />
-      <NewProducts />
-    </Main>
-  );
-}
+    <div>
+      <h1>Hello {props.name}</h1>
+      <hr />
+      {drawArray()}
+      <hr />
 
+      <ChildComponent />
+    </div>
+  )
+}
 export default App;
---
+++ src/container/ChildComponent.js
@@ -0,0 +1,5 @@
+function ChildComponent() {
+  return <h1>Child component</h1>
+}
+
+export default ChildComponent;
