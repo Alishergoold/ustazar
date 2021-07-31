d0b1c00d4c69b08d0333e25b90b7587921deb27b
Alishergoold <alishergoold1989@gmail.com>
2021-07-31 16:17:23 +0500

    Add FunctionalComponents

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import App from './container/App';
 
-
-//import styled from 'styled-components';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <Router>
+      <App name={'Amira'} />
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
