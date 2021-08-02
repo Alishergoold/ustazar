e0b87a27a13db2bc0cf04c65365ae03ec1aa584f
Alishergoold <alishergoold1989@gmail.com>
2021-08-02 18:19:45 +0500

    add useState

--- src/container/App.js
+++ src/container/App.js
@@ -61,24 +61,165 @@
 
 
 // Parent and child Component
-import ChildComponent from "./ChildComponent";
-function App(props) {
+//import ChildComponent from "./ChildComponent";
+//function App(props) {
 
-  function drawArray() {
-    return arr.map((item, index) => <p key={index}>It is {item}</p>)
-  }
+//  function drawArray() {
+//    return arr.map((item, index) => <p key={index}>It is {item}</p>)
+//  }
 
-  let arr = [1, 2, 3, 4, 5]
+//  let arr = [1, 2, 3, 4, 5]
 
-  return (
+//  return (
+//    <div>
+//      <h1>Hello {props.name}</h1>
+//      <hr />
+//      {drawArray()}
+//      <hr />
+//      <ChildComponent />
+//    </div>
+//  )
+//}
+//export default App;
+
+
+
+// {useState}
+//import {useState} from 'react';
+
+
+//function App(props){
+//  //state = {
+//  //  a:0
+//  //}
+//  const arr = useState(0);
+//  const a = arr[0];
+//  const setArrState = arr[1];
+
+//  function b(){
+//    setArrState(100)
+//  }
+
+//  return(
+//    <div>
+
+//      <h1>{a}</h1>
+
+//      <button onClick={b}>Counter</button>
+
+//    </div>
+//  )
+//}
+//export default App;
+
+
+//import {useState} from 'react';
+
+//function App(props){
+//  const [count, setCount] = useState(0);
+
+//  return(
+//    <div>
+
+//      <h1>{count}</h1>
+
+//      <button onClick={()=>setCount(count+1)}>+</button>
+
+//    </div>
+//  )
+//}
+//export default App;
+
+// useState count
+//import {useState} from 'react';
+//function App(props){
+//  const [count, setCount]=useState(0);
+
+
+//  return(
+//    <div>
+//      <h1>{count}</h1>
+//      <button onClick={()=>setCount(prev=>prev+1)}>+</button>
+//      <button onClick={()=>setCount(prev=>prev-1)}>-</button>
+//      <button onClick={()=>setCount(0)}>Reset</button>
+//    </div>
+//  )
+//}
+//export default App;
+
+
+//import {useState} from 'react';
+//function App(props){
+//  const [far, setFar] = useState(0)
+//  const [step, setStep] = useState(1)
+
+//  return(
+//    <div>
+//      <h1>Yurdi: {far}</h1>
+//      <button onClick={()=>setFar(prev=>prev+step)}>Yurmoq</button>
+//      <hr/>
+//      <h1> Qadam kattaligi: {step}</h1>
+//      <button onClick={()=>setStep(prev=>prev+1)}></button>
+//    </div>
+//  )
+//}
+//export default App;
+
+import {useState} from 'react';
+
+//function App(props){
+//  const [user, setUser] = useState({firstName:'aaa', lastName:'bbb'})
+
+//  function changeUser(){
+//    user.firstName = 'Alisher'
+//    let a = {...user}
+//    setUser(a)
+//    }
+
+//  return(
+//    <div>
+//      <h1> {user.firstName+' '+user.lastName}</h1>
+
+//      <button onClick={changeUser}>Change user</button>
+//    </div>
+//  )
+//}
+
+
+
+//Murakkab state
+
+
+
+function App(props){
+  const [user, setUser] = useState({
+    name: 'AAA',
+    address:{
+      country:{
+        city:{
+          street:{
+            name:'BBB'
+          }
+        }
+      }
+    }
+  });
+
+  function changeUser(){
+    user.address.country.city.street.name = 'CCC'
+    let a = {...user}
+    setUser(a)
+    }
+
+  return(
     <div>
-      <h1>Hello {props.name}</h1>
-      <hr />
-      {drawArray()}
-      <hr />
-
-      <ChildComponent />
+      <button onClick={changeUser}>Change user</button>
+     
+      {JSON.stringify(user, null, 2)}
     </div>
   )
 }
+
+
+
 export default App;
